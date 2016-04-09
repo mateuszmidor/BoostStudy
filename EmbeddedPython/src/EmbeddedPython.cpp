89d4f3f3cd9387a868c71e2d02c611dcfa9a1685
mateuszmidor <kontakt@mateuszmidor.com>
2016-04-09 20:12:38 +0200

    Added set_LD_LIBRARY_PATH.sh and .gitignore, added comments and refactored a little

--- EmbeddedPython/src/EmbeddedPython.cpp
+++ EmbeddedPython/src/EmbeddedPython.cpp
@@ -1,30 +1,26 @@
-//============================================================================
-// Name        : EmbeddedPython.cpp
-// Author      : Mateusz Midor
-// Version     :
-// Copyright   : 
-// Description : Hello World in C++, Ansi-style
-//============================================================================
+/**
+ * @file   EmbeddedPython.cpp
+ * @author Mateusz Midor, kontakt@mateuszmidor.com
+ * @date   IV 2016
+ * @brief  First demo of embedding python in c++ using Boost::Python
+ * @remark Do: export LD_LIBRARY_PATH=/usr/local/lib/boost/installation/lib/
 
-//#include <boost/python/detail/wrap_python.hpp>
+	This demo requires Boost::Python installed under:
+	/user/local/lib/boost/installation/lib/
+ */
+
 #include <boost/python.hpp>
 #include <iostream>
 
 using namespace boost::python;
 using namespace std;
 
-// First export LD_LIBRARY_PATH=/usr/local/lib/boost/installation/lib/
 int main() {
 	Py_Initialize();
 
 	object main_module = import("__main__");
 	object main_namespace = main_module.attr("__dict__");
 
-	object ignored = exec("hello = file('hello.txt', 'w')\n"
-	                      "hello.write('Hello world!')\n"
-	                      "hello.close()",
-	                      main_namespace);
-
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+	const char PYTHON[] = "print '--- hello from python! ---'";
+	object ignored = exec(PYTHON, main_namespace);
 }
